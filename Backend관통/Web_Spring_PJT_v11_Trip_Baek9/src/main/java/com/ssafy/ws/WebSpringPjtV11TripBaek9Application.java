e689df3efbd448f3f51be9e69867ec81136af0ae
전성권 <jeonsg9904@gmail.com>
2024-04-26 14:32:42 +0900

    first commit

---
+++ Backend관통/Web_Spring_PJT_v11_Trip_Baek9/src/main/java/com/ssafy/ws/WebSpringPjtV11TripBaek9Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringPjtV11TripBaek9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringPjtV11TripBaek9Application.class, args);
+	}
+
+}
