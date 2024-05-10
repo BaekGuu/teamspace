b8bdb0848a7c8668a124d5bcedefaac33ebb353c
전성권 <jeonsg9904@gmail.com>
2024-05-10 12:06:51 +0900

    comment table add && sql change

--- Backend관통/Web_Spring_PJT_v11_Trip_Baek9/src/main/resources/finalProject_trip_Baek9.sql
+++ Backend관통/Web_Spring_PJT_v11_Trip_Baek9/src/main/resources/finalProject_trip_Baek9.sql
@@ -33,10 +33,29 @@ create table if not exists board(
 );
 
 
+
 insert into board(writer_id,title,content)
 values
 ("jaeseung","frontend 담당 하람 누나","버스 감사요"),
 ("haram","front 1등","front 정도야 껌이지");
 
+create table if not exists comment(
+	comment_id int auto_increment primary key,
+    comment varchar(300) not null,
+    member_id varchar(30) not null,
+    board_id int not null,
+    writing_time datetime default current_timestamp, 
+    foreign key (member_id) references member(id) on delete cascade,
+    foreign key (board_id) references board(board_id) on delete cascade
+);
+
+insert into comment(comment, member_id, board_id)
+values
+("프론트 전문가는 다르네","jaeseung",1),
+("나만 믿으라구!!","haram",1),
+("Vue.js 정도는 쉽지","haram",2),
+("믿습니다 하멘","jaeseung",2);
+
 select * from board;
 select * from member;
+select * from comment;
